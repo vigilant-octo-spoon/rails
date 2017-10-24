532b8135cb30581c6a74f207062230299cbbaa18
cahenning <cahenning@uc.cl>
2017-10-23 23:35:41 -0300

    Fixed error in spec helper

--- spec/controllers/binnacle_controller_spec.rb
+++ spec/controllers/binnacle_controller_spec.rb
@@ -1,5 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe BinnacleController, type: :controller do
-
+  it "responds with JSON" do
+    expect(response).to be_success
+  end
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -12,8 +12,7 @@
 # the additional setup, and require it from the spec files that actually need
 # it.
 #
-g.mock_framework = :rspec
-end
+
 # See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
 RSpec.configure do |config|
 
