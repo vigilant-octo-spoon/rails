1c582ea87cfe973fac67578caabaafcb58dd3ca2
Unknown <jhidalgo1@uc.cl>
2017-11-03 16:31:44 -0300

    hotfix

--- app/controllers/follows_methodology_controller.rb
+++ app/controllers/follows_methodology_controller.rb
@@ -20,18 +20,18 @@ class FollowsMethodologyController < ApplicationController
         @broadcasts = Broadcast.where(follows_methodologies_id: fll.id).select(:id,:moment_of_implementation,:audience,:diffusion_channel,:objective)
         @conditions = Condition.where(follows_methodologies_id: fll.id).select(:id,:item,:info)
         @resources = Resource.where(follows_methodologies_id: fll.id).select(:id,:item,:available,:acquisition)
-        json_step3 = { :planning => @planning, :work_roles => @work_roles, :broadcasts => @broadcasts, :conditions => @conditions, :resources => @resources}.to_json
+        json_step3 = { :planning => @planning, :work_roles => @work_roles, :broadcasts => @broadcasts, :conditions => @conditions, :resources => @resources}
         #### STEP 4 #####
         @binnacles = Binnacle.where(follows_methodologies_id: fll.id).select(:id,:start_date,:finish_date,:objectives,:observations,:advances,:obstacles,:ideas)
-        json_step4 = { :binnacles => @binnacles}.to_json
+        json_step4 = { :binnacles => @binnacles}
         #### STEP 5 #####
         eva = Evaluation.where(follows_methodologies_id: fll.id).select(:id,:comments_connect,:comments_select,:comments_planning,:comments_implementation,:users_reflection,:users_suggestions).first
-        json_step5 = { :evaluation => eva}.to_json
+        json_step5 = { :evaluation => eva}
         #### STEP 6 #####
         rep = Report.where(follows_methodologies_id: fll.id).select(:id,:comment).first
-        json_step5 = { :report => rep}.to_json
+        json_step6 = { :report => rep}
 
-        json_follow= { :id => fll.id, :name => methodology_name, :step => fll.step, :step3 => json_step3, :ste4p => json_step4, :st5ep => json_step5, :s6tep => json_step6 }.to_json
+        json_follow= { :id => fll.id, :name => methodology_name, :step => fll.step, :step3 => json_step3, :step4 => json_step4, :step5 => json_step5, :step6 => json_step6 }
         follows.push(json_follow)
       end
       json_final = { :follows => follows}.to_json
